6aec18ecbb077e412410b04bdbbe632542d3cde7
web-padawan <iamkulykov@gmail.com>
2021-01-13 16:11:58 +0200

    Replace dom-module with registerStyles

--- theme/lumo/vaadin-form-layout-styles.js
+++ theme/lumo/vaadin-form-layout-styles.js
@@ -1,14 +1,12 @@
+import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import '@vaadin/vaadin-lumo-styles/spacing.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 
-const $_documentContainer = html`<dom-module id="lumo-form-layout" theme-for="vaadin-form-layout">
-  <template>
-    <style>
-      :host {
-        --vaadin-form-layout-column-spacing: var(--lumo-space-l);
-      }
-    </style>
-  </template>
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
+registerStyles(
+  'vaadin-form-layout',
+  css`
+    :host {
+      --vaadin-form-layout-column-spacing: var(--lumo-space-l);
+    }
+  `,
+  { moduleId: 'lumo-form-layout' }
+);
--- theme/material/vaadin-form-item-styles.js
+++ theme/material/vaadin-form-item-styles.js
@@ -1,21 +1,19 @@
+import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import '@vaadin/vaadin-material-styles/color.js';
 import '@vaadin/vaadin-material-styles/typography.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 
-const $_documentContainer = html`<dom-module id="material-form-item" theme-for="vaadin-form-item">
-  <template>
-    <style>
-      [part="label"] {
-        font-family: var(--material-font-family);
-        font-size: var(--material-small-font-size);
-        color: var(--material-secondary-text-color);
-        line-height: 16px;
-        font-weight: 400;
-        margin-top: 16px;
-        margin-bottom: 8px;
-      }
-    </style>
-  </template>
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
+registerStyles(
+  'vaadin-form-item',
+  css`
+    [part='label'] {
+      font-family: var(--material-font-family);
+      font-size: var(--material-small-font-size);
+      color: var(--material-secondary-text-color);
+      line-height: 16px;
+      font-weight: 400;
+      margin-top: 16px;
+      margin-bottom: 8px;
+    }
+  `,
+  { moduleId: 'material-form-item' }
+);
